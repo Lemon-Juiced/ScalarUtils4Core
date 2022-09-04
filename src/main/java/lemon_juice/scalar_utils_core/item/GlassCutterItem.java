044db8c610f3624a43cf9ffa85f541fc8dc47ac6
Lemon-Juiced <tbrotheawesome@gmail.com>
2022-09-04 01:44:40 -0400

    Gave Glass Cutter A Tooltip

--- src/main/java/lemon_juice/scalar_utils_core/item/GlassCutterItem.java
+++ src/main/java/lemon_juice/scalar_utils_core/item/GlassCutterItem.java
@@ -1,13 +1,20 @@
 package lemon_juice.scalar_utils_core.item;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.PickaxeItem;
 import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class GlassCutterItem extends PickaxeItem {
 
@@ -29,5 +36,11 @@ public class GlassCutterItem extends PickaxeItem {
         }
 
         return true;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components, TooltipFlag flag) {
+        components.add(Component.literal("WIP - Breaks Glass").withStyle(ChatFormatting.BOLD));
+        super.appendHoverText(stack, level, components, flag);
     }
 }
